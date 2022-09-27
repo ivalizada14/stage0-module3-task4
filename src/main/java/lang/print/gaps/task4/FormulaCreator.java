7ab5b86d265cfc442109e8a3148ca8e15dd53f43
Ibrahim Velizade <ivalizada14@gmail.com>
2022-09-27 17:55:04 +0300

    third attempt

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -2,8 +2,8 @@ package lang.print.gaps.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
-        int a = 1, b = 2;
+        int a , b;
         //int f = (9*a*a+5*b+2+a-7)*((a+b-4*a*b)/2);
-        System.out.printf("(9*%d^2+5*%d+2+%d-7)*((%d+%d-4*%d*%d)/2)", a,b,a,a,b,a,b);
+        System.out.println("(9*a^2+5*b+2+a-7)*((a+b-4*a*b)/2)");
     }
 }
