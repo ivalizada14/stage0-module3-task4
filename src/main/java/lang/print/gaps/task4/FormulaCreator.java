5a0a93c5b56bca5b67d89c05fea76bb29a9bfbdd
Ibrahim Velizade <ivalizada14@gmail.com>
2022-09-27 17:50:25 +0300

    first attempt

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -3,7 +3,7 @@ package lang.print.gaps.task4;
 public class FormulaCreator {
     public static void main(String[] args) {
         int a = 1, b = 2;
-        int f = (9*a*a+5*b+2+a-7)*((a+b-4*a*b)/2);
-        System.out.println(f);
+        //int f = (9*a*a+5*b+2+a-7)*((a+b-4*a*b)/2);
+        System.out.printf("(9*%d^2+5*%d+2+%d-7)*((%d+%d-4*%d*%d)/2)", a,b,a,a,b,a,b);
     }
 }
--- src/main/java/lang/print/gaps/task4/SettingTypes.java
+++ src/main/java/lang/print/gaps/task4/SettingTypes.java
@@ -3,7 +3,7 @@ package lang.print.gaps.task4;
 public class SettingTypes {
     public static void main(String[] args) {
          long first = 10_000_000_000L;
-         byte second = 'a';
+         char second = 'a';
          boolean third = 5 > 9;
          double forth = 12.90434323324342;
          float fifth = 12.3F;
